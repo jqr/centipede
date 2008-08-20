b9b749399f3696daae8c40f8ccc8d423d29828a4
Elijah Miller <elijah.miller@gmail.com>
2008-08-20 19:19:41 -0400

    Adding basic sound loading and playing.

--- lib/centipede.rb
+++ lib/centipede.rb
@@ -5,6 +5,7 @@ $: << File.join(File.dirname(__FILE__))
 require 'centipede/gosu_extras'
 require 'centipede/window'
 
+RESOURCE_PATH = File.join(File.dirname(__FILE__), '..')
 class Centipede
   def self.run
     Window.new.show
--- lib/centipede/window.rb
+++ lib/centipede/window.rb
@@ -1,8 +1,24 @@
 class Window < Gosu::Window
+  attr_accessor :sounds
+  
   def initialize
     # double size
     super(480, 512, false)
     GosuExtras::setup_keyboard_constants(self)
+    load_sounds
+  end
+  
+  def load_sounds
+    self.sounds = {}
+    Dir.glob(File.join(RESOURCE_PATH, 'sounds', '*')).each do |file|
+      sounds[File.basename(file, '.wav')] = Gosu::Sample.new(self, file)
+    end
+  end
+
+  def play_sound(sound)
+    if sounds[sound]
+      sounds[sound].play
+    end
   end
   
   def update
