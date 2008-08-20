15f1b809708d4b00b5d4793702a53d0cc0971349
kris <kristopher.chambers@gmail.com>
2008-08-20 19:08:21 -0400

    Adding blank update and draw methods to window.

--- lib/centipede/window.rb
+++ lib/centipede/window.rb
@@ -5,6 +5,12 @@ class Window < Gosu::Window
     GosuExtras::setup_keyboard_constants(self)
   end
   
+  def update
+  end
+  
+  def draw
+  end
+  
   def button_down(id)
     case id
     when Gosu::Button::KbEscape
