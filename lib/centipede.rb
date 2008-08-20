3260b87f8bfd1d721af1ff0465839676e8f8e966
Chris Zelenak <shade@shade-laptop.(none)>
2008-08-20 19:35:11 -0400

    remove RESOURCE_DIR from centipede

--- lib/centipede.rb
+++ lib/centipede.rb
@@ -3,9 +3,10 @@ require 'gosu'
 
 $: << File.join(File.dirname(__FILE__))
 require 'centipede/gosu_extras'
+require 'centipede/assets'
 require 'centipede/window'
 
-RESOURCE_PATH = File.join(File.dirname(__FILE__), '..')
+
 class Centipede
   def self.run
     Window.new.show
