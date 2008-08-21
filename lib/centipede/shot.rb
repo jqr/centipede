4cff45e9373836ab94bf3f09fd51ce40f9e894d4
Elijah Miller <elijah.miller@gmail.com>
2008-08-20 22:28:33 -0400

    Mushroom shootage.

--- lib/centipede/shot.rb
+++ lib/centipede/shot.rb
@@ -2,6 +2,7 @@ class Shot
   Z_ORDER = 4
 
   def initialize(window, x, y)
+    @window = window
     @x = x
     @y = y
     @@image ||= Gosu::Image.new(window, File.join(GAME_DIR, 'images', 'shot.png'))
@@ -14,7 +15,22 @@ class Shot
   def update(ts)
     @y = @y - 20
 
-    if @y < -6
+    if off_screen?
+      Game.current_game.remove_shot
+    end
+    
+    check_for_hit
+  end
+  
+  def off_screen?
+    @y < -6
+  end
+  
+  def check_for_hit
+    grid_x, grid_y = @x / 16, @y / 16
+    unless Game.current_game.level.open?(@x / 16, @y / 16)
+      health = Game.current_game.level.grid[grid_y][grid_x] - 1
+      Game.current_game.level.grid[grid_y][grid_x] = health > 0 ? health : nil 
       Game.current_game.remove_shot
     end
   end
