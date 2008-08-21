43fd31443d607e0203a62d64fb6ab625df8ce014
Chris Zelenak <shade@shade-laptop.(none)>
2008-08-21 12:16:10 -0400

    change check_for_hit to handle multiple levels of hit detection in same enemies loop

--- lib/centipede/shot.rb
+++ lib/centipede/shot.rb
@@ -22,33 +22,41 @@ class Shot
       Game.current_game.remove_shot
     end
     
-    check_for_hit(@x, @y + 14) ||
-    check_for_hit(@x, @y + 7) ||
-    check_for_hit(@x, @y)
+    check_for_hit(@x, @y, 2, 7) 
   end
   
   def off_screen?
     @y < -6
   end
   
-  def check_for_hit(x, y)
+  def check_for_hit(x, y, steps = 0, step_size = 7)
     Game.current_game.enemies.each do |enemy|
-      if enemy.collides_with?(x, y)
-        enemy.hit
+      enemy_hit = false
+      for i in (0..steps)
+        if !enemy_hit && enemy.collides_with?(x, y + (i*step_size))
+          enemy.hit
+          enemy_hit = true
+          break
+        end
+      end
+      if enemy_hit
         Game.current_game.remove_shot
         return true
       end
     end
 
-    grid_x, grid_y = x / 16, y / 16
-    if Game.current_game.level.grid[grid_y]
-      unless Game.current_game.level.open?(grid_x, grid_y)
-        health = Game.current_game.level.grid[grid_y][grid_x] - 1
-        Game.current_game.level.grid[grid_y][grid_x] = health > 0 ? health : nil 
-        Game.current_game.remove_shot
-        return true
-      end
+    for i in (0..steps)
+      grid_x, grid_y = x / 16, (y + i * step_size) / 16
+      if Game.current_game.level.grid[grid_y]
+        unless Game.current_game.level.open?(grid_x, grid_y)
+          health = Game.current_game.level.grid[grid_y][grid_x] - 1
+          Game.current_game.level.grid[grid_y][grid_x] = health > 0 ? health : nil 
+          Game.current_game.remove_shot
+          return true
+        end
+      end      
     end
+
     
     false
   end
