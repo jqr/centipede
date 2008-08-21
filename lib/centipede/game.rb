cdb2f300cb534e41ccef11777167fba779ceca2b
Chris Zelenak <shade@shade-laptop.(none)>
2008-08-20 20:15:43 -0400

    add game object

---
+++ lib/centipede/game.rb
@@ -0,0 +1,61 @@
+class Game
+
+  attr_accessor :score, :player, :enemies
+
+  def self.current_game
+    @game
+  end
+
+  def initialize(window, player)
+    @start_time = nil
+    @end_time = nil
+    @window = window
+    self.enemies = []
+    Game.current_game = self
+  end
+
+  def start
+    @start_time = Time.now.to_i
+    @pause = false
+  end
+
+  def pause
+    @paused = !@paused
+  end
+
+  def stop
+    @end_time = Time.now
+  end
+
+  def paused?
+    @paused
+  end
+
+  def in_play?
+    !(@paused || @end_time)
+  end
+
+  def run(ts=nil)
+    ts ||= Time.now
+    if self.in_play?
+      self.enemies.each do |e|
+        e.update(ts)
+      end
+      self.player.update(ts)
+    end
+  end
+
+  def draw
+    self.enemies.each do |e|
+      e.draw
+    end
+    self.player.draw
+  end
+
+  private
+
+  def self.current_game=(g)
+    @game = g
+  end
+  
+end
